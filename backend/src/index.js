574b45c33aafa7e032ee8f631a02dd43689ec577
Elise Frazier <efrazie7@uncc.edu>
2024-09-03 13:48:43 -0400

    aded back frontend and backend folders for vercel deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import customers from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: `https://bbycakes.vercel.app/`, credentials: true }));
 app.use(express.json());
 app.use("/record", customers);
 
@@ -15,13 +15,18 @@ app.listen(PORT, () => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
 }
+
+module.exports = app;
